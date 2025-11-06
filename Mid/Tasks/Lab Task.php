c6685c27a11c79292532e0ee68c6a809fe996bf6
Schizo-cell <amanahnaf700@gmail.com>
2025-11-06 10:38:14 +0600

    Add files via upload

---
+++ Mid/Tasks/Lab Task.php
@@ -0,0 +1,114 @@
+<!Doctype html>
+<html>
+    <center>
+<head>
+    <h1>
+        Clinic Patient Registration
+    </h1>
+</head>    
+</center>
+
+<center>
+<body>
+
+<table>
+<tr>
+   <td>
+   Full Name: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Age: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Phone Number: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Email Address: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Insurance Provider: <br>
+   </td>
+   <td>
+    <select>
+   <option> Select Provider </option>
+</select>
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Insurance Policy Number: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+</table>
+
+<head>
+    <h1>
+        Additional Information
+    </h1>
+</head>
+
+<table>
+<tr>
+   <td>
+   Username: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Password: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+
+<tr>
+   <td>
+   Confirm Password: <br>
+   </td>
+   <td>
+   <input type= "text">
+   </td>
+</tr>
+<tr>
+   <td>
+ <input type= "submit" value = "Register">
+  </td>
+</tr>
+</table>
+
+</body>
+</center>
