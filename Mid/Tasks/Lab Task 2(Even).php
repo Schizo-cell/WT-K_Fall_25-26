01921fddefd2571b3756d6f9efe16d16daa4009c
Schizo-cell <amanahnaf700@gmail.com>
2026-01-02 19:10:59 +0600

    This is my first change

--- Mid/Tasks/Lab Task 2(Even).php
+++ Mid/Tasks/Lab Task 2(Even).php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Participant Registration Section</title>
+    <title>Participantssss Registration Section</title>
     <style>
         body {
             font-family: Arial, sans-serif;
